06df8c3ed66d274f6ec3ab1bf6cbb9ff419dc3f5
Metin Lamby <zsh git_setup.sh>
2020-05-21 23:15:07 +0200

    adedd some pictures

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
     new Typed('#banner-typed-text', {
-    strings: ["user experience", "design", "brands", "products", "everything" ],
+    strings: ["user experience", "design", "brands", "products", "everything!" ],
     typeSpeed: 50,
     loop: true
   });
