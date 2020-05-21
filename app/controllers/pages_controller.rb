5e2df1e85c68f250285f74abac201fc8c9a24edc
Metin Lamby <zsh git_setup.sh>
2020-05-21 23:24:40 +0200

    adedd some pictures two

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,12 @@ class PagesController < ApplicationController
   def home
     @subscriber = Subscriber.new # needed to instantiate the form_for
   end
+
+  def about
+    @subscriber = Subscriber.new # needed to instantiate the form_for
+  end
+
+  def contact
+    @subscriber = Subscriber.new # needed to instantiate the form_for
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'pages/contact'
+  get 'pages/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "subscribers/new", to: "subscribers#new" # The `new` route needs to be *before* `show` route.
   post "subscribers", to: "subscribers#create"
