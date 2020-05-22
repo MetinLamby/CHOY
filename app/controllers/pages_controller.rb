579d24977547710d34caa99574dbf078ab01029a
Metin Lamby <zsh git_setup.sh>
2020-05-22 09:27:22 +0200

    added the new page on different oage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,29 +1,10 @@
 class PagesController < ApplicationController
   def home
-    @subscriber = Subscriber.new # needed to instantiate the form_for
   end
 
   def about
-    @subscriber = Subscriber.new # needed to instantiate the form_for
   end
 
   def contact
-    @subscriber = Subscriber.new # needed to instantiate the form_for
   end
-
-  def create
-    @subscriber = Subscriber.new(subscriber_params)
-    if @subscriber.save
-      redirect_to root_path
-    else
-      render :home
-    end
-  end
-
-  private
-
-  def subscriber_params
-    params.require(:subscriber).permit(:email)
-  end
-
 end
