46b6764fbe4888f294a78b6cacb9392019523846
Metin Lamby <zsh git_setup.sh>
2020-05-25 00:16:28 +0200

    added the instruction

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,7 @@ class PagesController < ApplicationController
 
   def contact
   end
+
+  def instructions
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'pages/contact'
   get 'pages/about'
+  get 'pages/instructions'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "subscribers/new", to: "subscribers#new"
